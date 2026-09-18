30767422f14e5fbb5dbbcdb84a3da2dab8d71d01
Julio Cesar Montalvan <julio030902@gmail.com>
2026-09-18 17:54:38 -0500

    Corregir espaciado del titulo

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -1,6 +1,4 @@
-//import portraitImg from '../../assets/images/6qgycns9zfbb1.jpg';
 import Button from '../ui/Button';
-//mport PixelDust from '../ui/PixelDust';
 import Marquee from './Marquee';
 
 function Hero() {
@@ -9,7 +7,9 @@ function Hero() {
       
       {/* Headline */}
       <h1 className="font-pixel text-3xl leading-tight text-white sm:text-4xl md:text-6xl lg:text-7xl">
-        JULIO CESAR MONTALVAN
+        JULIO CESAR
+        <br/>
+        MONTALVAN 
       </h1>
 
       {/* CTA */}
